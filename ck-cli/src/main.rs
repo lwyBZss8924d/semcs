bfbdd3670cbf54e3b05bb6db820636addda34386
ivfiev <filjko@ya.ru>
2025-09-13 19:15:27 +0100

    undo logging change

--- ck-cli/src/main.rs
+++ ck-cli/src/main.rs
@@ -455,7 +455,7 @@ async fn run_main() -> Result<()> {
     tracing_subscriber::fmt()
         .with_env_filter(
             tracing_subscriber::EnvFilter::from_default_env()
-                .add_directive(tracing::Level::DEBUG.into()),
+                .add_directive(tracing::Level::WARN.into()),
         )
         .init();
 
